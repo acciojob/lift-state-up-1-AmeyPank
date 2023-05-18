47f0c589381a2492068a891ed45c6f05e48323c9
AmeyPank <pankantiameya@gmail.com>
2023-05-19 01:45:29 +0530

    <h1>Modal Content</h1>

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const ParentComponent = () => {
+const Parent = () => {
   const [showModal, setShowModal] = useState(false);
 
   const handleButtonClick = () => {
@@ -9,7 +9,7 @@ const ParentComponent = () => {
 
   return (
     <div className="parent">
-      <h2>Parent Component</h2>
+      <h1>Parent Component</h1>
       <ChildComponent showModal={showModal} handleButtonClick={handleButtonClick} />
     </div>
   );
@@ -18,7 +18,7 @@ const ParentComponent = () => {
 const ChildComponent = ({ showModal, handleButtonClick }) => {
   return (
     <div className="child">
-      <h3>Child Component</h3>
+      <h1>Modal Content</h1>
       <button onClick={handleButtonClick}>Show Modal</button>
       {showModal && <Modal />}
     </div>
@@ -28,10 +28,10 @@ const ChildComponent = ({ showModal, handleButtonClick }) => {
 const Modal = () => {
   return (
     <div className="modal">
-      <h4>Modal</h4>
+      <h1>Modal</h1>
       <p>This is a modal component.</p>
     </div>
   );
 };
 
-export default ParentComponent;
+export default Parent;
