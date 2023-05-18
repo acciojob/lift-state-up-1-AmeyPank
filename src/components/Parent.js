17457e82358ee2abc280477c45d5e9a2be8c34f3
AmeyPank <pankantiameya@gmail.com>
2023-05-19 01:48:29 +0530

    <h3>Modal</h3>

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -28,7 +28,7 @@ const ChildComponent = ({ showModal, handleButtonClick }) => {
 const Modal = () => {
   return (
     <div className="modal">
-      <h1>Modal</h1>
+      <h3>Modal</h3>
       <p>This is a modal component.</p>
     </div>
   );
